22ca12fb67c664750616ccb4dc8d27eeb7a3c32b
최형동 <dong@choehyeongdong-ui-MacBookPro.local>
2020-03-17 22:40:24 +0900

    Error of useConfirm fixed

--- src/useConfirm.js
+++ src/useConfirm.js
@@ -1,8 +1,7 @@
 export const useConfirm = (message ="", onConfirm, onCancel) =>{
-    if(typeof callback !== "function" || typeof onCancel !== "function"){
+    if(typeof onConfirm !== "function" || typeof onCancel !== "function"){
       return;
     }
-    
     const confirmAction = () =>{
       if(window.confirm(message)){
         onConfirm();
